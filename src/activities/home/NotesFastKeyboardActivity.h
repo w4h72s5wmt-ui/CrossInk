9867c4e573bd11f661a8450d7287dd099fecf4df
w4h72s5wmt-ui <w4h72s5wmt@privaterelay.appleid.com>
2026-09-26 17:50:53 +0200

    notes: fix predictive text include path

--- src/activities/home/NotesFastKeyboardActivity.h
+++ src/activities/home/NotesFastKeyboardActivity.h
@@ -10,7 +10,7 @@
 #include "activities/Activity.h"
 #include "activities/util/KeyboardEntryActivity.h"
 #include "util/ButtonNavigator.h"
-#include "PredictiveText.h"
+#include "activities/util/PredictiveText.h"
 
 // Text entry on the FreeInkUI keyboard component: the SDK layout tables and
 // keyboard() do the key rendering and hit-rect registration, InteractionBuffer
